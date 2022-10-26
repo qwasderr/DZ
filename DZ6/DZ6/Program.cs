1ddc160895e456bb07334c29e45aa686c17f9628
Max Markov <99619748+qwasderr@users.noreply.github.com>
2022-10-26 19:15:14 +0300

    Add files via upload

---
+++ DZ6/DZ6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using (FileStream fs = new FileStream("C:/json.json", FileMode.OpenOrCreate))
+{
+   
+    var book =  await JsonSerializer.DeserializeAsync<List<Book>>(fs);
+    foreach (var b in book)
+    {
+        Console.WriteLine($"{b.PublishingHouseId} - {b.Title} - {b.PublishingHouse.Id} - {b.PublishingHouse.Name} - {b.PublishingHouse.Adress}");
+    }
+}
+
+class Books
+{
+    public List<Book> data { get; set; }
+}
+public class Book
+{
+
+    public int PublishingHouseId { get; set; }
+    //public int PublishingHouseId; - без серіалізації
+    //[JsonPropertyName("Name")]; - зміна ім'я поля
+    public string Title { get; set; }
+    public PublishingHouse PublishingHouse { get; set; }
+    //public PublishingHouse PublishingHouse - без десеріалізації 
+    public Book(int publishingHouseId, string title)
+    {
+        PublishingHouseId = publishingHouseId;
+        Title = title;
+        PublishingHouse = new PublishingHouse();
+        
+    }
+
+
+}
+public class PublishingHouse
+{
+   
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Adress { get; set; }
+   
+}
