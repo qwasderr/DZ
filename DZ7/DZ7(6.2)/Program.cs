f3ac0c650c6b76d117befe91a7321eb63c564c45
Max Markov <99619748+qwasderr@users.noreply.github.com>
2022-11-09 21:08:21 +0200

    Add files via upload

---
+++ DZ7/DZ7(6.2)/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class prog
+{
+    static void Main()
+    {
+        var sys2 = new System2();
+        Controller.Out(sys2);
+        var sys1 = new System1();
+        // Controller.Out(sys1); error
+        var adapter = new Adapter(sys1);
+        Controller.Out(adapter);
+    }
+}
+class System1
+{
+    public void NonSpecialOutput()
+    {
+        Console.WriteLine("This is a non special output");
+    }
+}
+interface ISpecial
+{
+    void SpecialOutput();
+}
+class System2: ISpecial
+{
+    public void SpecialOutput()
+    {
+        Console.WriteLine("This is a special output");
+    }
+}
+class Adapter: ISpecial
+{
+    private System1 sys1;
+    public Adapter(System1 sys1)
+    {
+        this.sys1 = sys1;
+    }
+    public void SpecialOutput()
+    {
+        sys1.NonSpecialOutput();
+    }
+}
+class Controller
+{
+    public static void Out(ISpecial i)
+    {
+        i.SpecialOutput();
+    }
+}
