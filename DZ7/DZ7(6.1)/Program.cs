9498a9299893bb6f120a0992ca82c9c1e7351782
Max Markov <99619748+qwasderr@users.noreply.github.com>
2022-11-09 21:07:38 +0200

    Add files via upload

---
+++ DZ7/DZ7(6.1)/Program.cs
@@ -0,0 +1,63 @@
+﻿class prog
+{
+    static void Main()
+    {
+        Facade fac = new Facade();
+        fac.Replenishment();
+        fac.ReceiveMoney();
+    }
+}
+class ReceiveCreditCard
+{
+    public void ReceiveCC()
+    {
+        Console.WriteLine("A credit Card has been inserted");
+    }
+}
+class MoneyOp
+{
+    public void AmountOfMoney()
+    {
+        Console.WriteLine("An amount of money has been entered");
+    }
+}
+class ReplenishmentOp
+{
+    public void Replenishment()
+    {
+        Console.WriteLine("Replenishment has been made");
+    }
+}
+class MoneyOut
+{
+    public void Money_Out()
+    {
+        Console.WriteLine("Money has been given to the user");
+    }
+}
+class Facade
+{
+    ReceiveCreditCard rcc;
+    MoneyOp mo;
+    ReplenishmentOp ro;
+    MoneyOut moout;
+    public Facade()
+    {
+        rcc = new ReceiveCreditCard();
+        mo=new MoneyOp();
+        ro=new ReplenishmentOp();
+        moout = new MoneyOut();
+    }
+    public void Replenishment()
+    {
+        rcc.ReceiveCC();
+        mo.AmountOfMoney();
+        ro.Replenishment();
+    }
+    public void ReceiveMoney()
+    {
+        rcc.ReceiveCC();
+        mo.AmountOfMoney();
+        moout.Money_Out();
+    }
+}
