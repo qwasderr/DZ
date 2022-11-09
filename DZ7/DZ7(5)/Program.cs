9cc13115a66ab7d3ee63f2abde022aba2f4b21af
Max Markov <99619748+qwasderr@users.noreply.github.com>
2022-11-09 21:06:58 +0200

    Add files via upload

---
+++ DZ7/DZ7(5)/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PrototypeFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            IFigure figure = new Rectangle(10, 20);
+            IFigure clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+            figure = new Circle(15);
+            clonedFigure = figure.Clone();
+            figure.GetInfo();
+            clonedFigure.GetInfo();
+
+            figure = new Triangle(15, 47);
+            figure.GetInfo();
+            clonedFigure = figure.Clone();
+            clonedFigure.GetInfo();
+            Console.Read();
+        }
+    }
+    interface IFigure
+    {
+        IFigure Clone();
+        void GetInfo();
+    }
+    class Rectangle : IFigure
+    {
+        int width;
+        int height;
+        public Rectangle(int w, int h)
+        {
+            width = w;
+            height = h;
+        }
+        public IFigure Clone()
+        {
+            return new Rectangle(this.width, this.height);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Прямокутник довжиною {0} и шириною{1}", height, width);
+        }
+    }
+    class Circle : IFigure
+    {
+        int radius;
+        public Circle(int r)
+        {
+            radius = r;
+        }
+        public IFigure Clone()
+        {
+            return new Circle(this.radius);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Круг радіусом {0}", radius);
+        }
+    }
+    class Triangle : IFigure
+    {
+        int per, area;
+        public Triangle(int per, int area)
+        {
+            this.per = per;
+            this.area = area;
+        }
+        public IFigure Clone()
+        {
+            return new Triangle(this.per, this.area);
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Трикутник периметром {0} та площею {1}", per,area);
+        }
+
+    }
+}
